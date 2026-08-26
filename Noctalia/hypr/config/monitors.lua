f1bcb6a8bc84ddda61b0fd883eb399707a98a0e6
Jeremy Pham <jeremy.pham420@gmail.com>
2026-08-25 18:56:02 -0700

    Fixing monitor setups for class/home

--- Noctalia/hypr/config/monitors.lua
+++ Noctalia/hypr/config/monitors.lua
@@ -7,15 +7,27 @@
 --     scale     = "1",
 -- })
 
-hl.monitor({
-	output = MONITOR1,
-	mode = "preferred",
-	position = "0x0",
-	scale = "auto",
-})
-
+-- Home PC Main Monitor
 hl.monitor({
 	output = "DP-3",
 	mode = "1920x1080@165",
 	position = "1920x0",
+})
+
+-- Home PC Side Monitor
+
+-- Laptop setup
+hl.monitor({
+	output = "eDP-1",
+	mode = "3200x2000@120",
+	position = "0x0",
+	scale = 2,
+})
+
+-- Prefer above automatically since for laptop we usually go up
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto-up",
+	scale = "auto",
 })
